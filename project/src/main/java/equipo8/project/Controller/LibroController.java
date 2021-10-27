b4bee614fb0c4144187b97ecb7422fe264f9de35
diego <203414@ids.upchiapas.edu.mx>
2021-10-27 17:38:31 -0500

    implementacion de metodo agregar

--- project/src/main/java/equipo8/project/Controller/LibroController.java
+++ project/src/main/java/equipo8/project/Controller/LibroController.java
@@ -4,6 +4,8 @@ import equipo8.project.Repository.LibroRepository;
 import equipo8.project.model.Libro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,10 @@ public class LibroController {
     @GetMapping(value = "/listaLibro")
     public List<Libro> getListLibro(){
         return libroRepository.findAll();
+    }
+    @PostMapping(value = "/libro/new")
+    public Libro addLibro(@RequestBody Libro libro){
+        return libroRepository.save(libro);
     }
 
 
--- project/src/main/java/equipo8/project/Repository/LibroRepository.java
+++ project/src/main/java/equipo8/project/Repository/LibroRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface LibroRepository extends CrudRepository<Libro, String>{
     List<Libro> findAll();
+    Libro save(Libro libro);
 }
